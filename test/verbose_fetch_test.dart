3c728cf09b2433cac2c16b1e874f19d7a958c10c
PratchayaKhunsee <pratchaya.41.3943@gmail.com>
2021-08-23 16:01:49 +0700

    Change package description

--- test/verbose_fetch_test.dart
+++ test/verbose_fetch_test.dart
@@ -2,8 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:verbose_fetch/verbose_fetch.dart';
 
 void main() {
-  test('Doing HTTP request', () {
+  String url = 'https://popcat.click';
+  test('Doing HTTP request on ', () {
     // Put the URL here.
-    fetch('https://dart-simple-fetch-test-ws.herokuapps.com/');
+    fetch(url);
   });
 }
